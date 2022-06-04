967bda539a1d3837a507b084fd1bdb791386089a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-04 16:53:25 +0000

    Sat, Jun 4, 2022, 4:53 PM +00:00

--- kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
+++ kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
@@ -15,84 +15,18 @@ int main(void)
     printf("Enter an integer: ");
     scanf("%i", &value);
 
-    while (value >= 10)
+    // Counts digits in integer
+    short count = 0, copy = value;
+    while (copy != 0)
     {
-        short digit = value / 10;
-        value %= 10;
-    
-        switch (digit)
-        {
-            case 0:
-                printf("Zero ");
-                break;
-            case 1:
-                printf("One ");
-                break;
-            case 2:
-                printf("Two ");
-                break;
-            case 3:
-                printf("Three ");
-                break;
-            case 4:
-                printf("Four ");
-                break;
-            case 5:
-                printf("Five ");
-            case 6:
-                printf("Six ");
-                break;
-            case 7:
-                printf("Seven ");
-                break;
-            case 8:
-                printf("Eight ");
-                break;
-            case 9:
-                printf("Nine ");
-                break;
-            default:
-                printf("Invalid entry\n");
-                break;
-        }
+        short i = copy % 10;
+        count++;
+        copy /= 10;
     }
 
-    switch (value)
-    {
-        case 0:
-            printf("Zero ");
-            break;
-        case 1:
-            printf("One ");
-            break;
-        case 2:
-            printf("Two ");
-            break;
-        case 3:
-            printf("Three ");
-            break;
-        case 4:
-            printf("Four ");
-            break;
-        case 5:
-            printf("Five ");
-        case 6:
-            printf("Six ");
-            break;
-        case 7:
-            printf("Seven ");
-            break;
-        case 8:
-            printf("Eight ");
-            break;
-        case 9:
-            printf("Nine ");
-            break;
-        default:
-            printf("Invalid entry\n");
-            break;
-    }
-    putchar('\n');
+    printf("count = %i\n", count);
 
     return 0;
 }
+
+    
