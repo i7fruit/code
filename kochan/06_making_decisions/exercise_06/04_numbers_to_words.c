324ae569fb6b4f96b9c004cd60ce5d1dae4593b3
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-04 16:42:57 +0000

    Sat, Jun 4, 2022, 4:42 PM +00:00

--- kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
+++ kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
@@ -15,7 +15,7 @@ int main(void)
     printf("Enter an integer: ");
     scanf("%i", &value);
 
-    while (value > 0)
+    while (value > 10)
     {
         short digit = value / 10;
         value %= 10;
@@ -55,6 +55,42 @@ int main(void)
                 printf("Invalid entry\n");
                 break;
         }
+    }
+
+    switch (digit)
+    {
+        case 0:
+            printf("Zero ");
+            break;
+        case 1:
+            printf("One ");
+            break;
+        case 2:
+            printf("Two ");
+            break;
+        case 3:
+            printf("Three ");
+            break;
+        case 4:
+            printf("Four ");
+            break;
+        case 5:
+            printf("Five ");
+        case 6:
+            printf("Six ");
+            break;
+        case 7:
+            printf("Seven ");
+            break;
+        case 8:
+            printf("Eight ");
+            break;
+        case 9:
+            printf("Nine ");
+            break;
+        default:
+            printf("Invalid entry\n");
+            break;
     }
     putchar('\n');
 
