d921cc84527681b9ea1f494577a392474f9a0dc3
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-04 16:59:49 +0000

    Sat, Jun 4, 2022, 4:59 PM +00:00

--- kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
+++ kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
@@ -25,7 +25,58 @@ int main(void)
         copy /= 10;
     }
 
-    printf("count = %i\n", count);
+    // Computes divisor
+    int divisor = 1;
+
+    for (short i = 1; i < count; i++)
+    {
+        divisor *= 10;
+    }
+
+    printf("Divisor = %i\n", divisor);
+
+    //while (value > 0)
+    //{
+    //    short digit = value / 10;
+    //    value %= 10;
+    
+    //    switch (digit)
+    //    {
+    //        case 0:
+    //            printf("Zero ");
+    //            break;
+    //        case 1:
+    //            printf("One ");
+    //            break;
+    //        case 2:
+    //            printf("Two ");
+    //            break;
+    //        case 3:
+    //            printf("Three ");
+    //            break;
+    //        case 4:
+    //            printf("Four ");
+    //            break;
+    //        case 5:
+    //            printf("Five ");
+    //        case 6:
+    //            printf("Six ");
+    //            break;
+    //        case 7:
+    //            printf("Seven ");
+    //            break;
+    //        case 8:
+    //            printf("Eight ");
+    //            break;
+    //        case 9:
+    //            printf("Nine ");
+    //            break;
+    //        default:
+    //            printf("Invalid entry\n");
+    //            break;
+    //    }
+    //}
+    //putchar('\n');
 
     return 0;
 }
