56b9a7695aa82fa19618f753bd8e7865a4e80c2b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-05 04:52:12 +0000

    Sun, Jun 5, 2022, 4:52 AM +00:00

--- kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
+++ kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
@@ -33,48 +33,56 @@ int main(void)
         divisor *= 10;
     }
 
-    while (value >= 0)
+    while (value > 0)
     {
-        short digit = value / divisor;
-        // Resets the value
-        value %= divisor;
-        // Resets the divisor
-        divisor /= 10;
-    
-        switch (digit)
+        if (value != 0)
         {
-            case 0:
-                printf("Zero ");
-                break;
-            case 1:
-                printf("One ");
-                break;
-            case 2:
-                printf("Two ");
-                break;
-            case 3:
-                printf("Three ");
-                break;
-            case 4:
-                printf("Four ");
-                break;
-            case 5:
-                printf("Five ");
-            case 6:
-                printf("Six ");
-                break;
-            case 7:
-                printf("Seven ");
-                break;
-            case 8:
-                printf("Eight ");
-                break;
-            case 9:
-                printf("Nine ");
-                break;
-            default:
-                printf("Invalid entry\n");
-                break;
+            short digit = value / divisor;
+            // Resets the value
+            value %= divisor;
+            // Resets the divisor
+            divisor /= 10;
+        
+            switch (digit)
+            {
+                case 0:
+                    printf("Zero ");
+                    break;
+                case 1:
+                    printf("One ");
+                    break;
+                case 2:
+                    printf("Two ");
+                    break;
+                case 3:
+                    printf("Three ");
+                    break;
+                case 4:
+                    printf("Four ");
+                    break;
+                case 5:
+                    printf("Five ");
+                case 6:
+                    printf("Six ");
+                    break;
+                case 7:
+                    printf("Seven ");
+                    break;
+                case 8:
+                    printf("Eight ");
+                    break;
+                case 9:
+                    printf("Nine ");
+                    break;
+                default:
+                    printf("Invalid entry\n");
+                    break;
+            }
+        }
+        else
+        {
+            printf("Zero ");
+            break;
         }
     }
 
