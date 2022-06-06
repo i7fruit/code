042aa31c03e274e04b97c6b9ebf634f334689b63
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-06 01:51:25 +0000

    Mon, Jun 6, 2022, 1:51 AM +00:00

--- kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
+++ kochan/06_making_decisions/exercise_06/04_numbers_to_words.c
@@ -1,8 +1,8 @@
-/** 
+/**
  * 04_numbers_to_words.c
- * 
+ *
  * Saturday, June 04 2022 10:42hrs
- * 
+ *
  * This program gets an integer from the user
  * and prints the digits out in words to the screen.
  */
@@ -20,7 +20,7 @@ int main(void)
 
     // Counts digits in integer
     short count = 0;
-    
+
     while (copy != 0)
     {
         short i = copy % 10;
@@ -36,57 +36,63 @@ int main(void)
         divisor *= 10;
     }
 
-    while (value >= 0 && divisor > 0)
+    while (count > 0)
     {
-        short digit = value / divisor;
-
-        // Resets the value
-        value %= divisor;
-
-        // Resets the divisor
-        divisor /= 10;
-        
-        // Prints the digit in words
-        switch (digit)
+        if (value > 0 && count > 0)
         {
-            case 0:
-                printf("Zero ");
-                break;
-            case 1:
-                printf("One ");
-                break;
-            case 2:
-                printf("Two ");
-                break;
-            case 3:
-                printf("Three ");
-                break;
-            case 4:
-                printf("Four ");
-                break;
-            case 5:
-                printf("Five ");
-            case 6:
-                printf("Six ");
-                break;
-            case 7:
-                printf("Seven ");
-                break;
-            case 8:
-                printf("Eight ");
-                break;
-            case 9:
-                printf("Nine ");
-                break;
-            default:
-                printf("Invalid entry\n");
-                break;
+            short digit = value / divisor;
+            // Resets the value
+            value %= divisor;
+
+            // Resets the divisor
+            divisor /= 10;
+
+            // Prints the digit in words
+            switch (digit)
+            {
+                case 0:
+                    printf("Zero ");
+                    break;
+                case 1:
+                    printf("One ");
+                    break;
+                case 2:
+                    printf("Two ");
+                    break;
+                case 3:
+                    printf("Three ");
+                    break;
+                case 4:
+                    printf("Four ");
+                    break;
+                case 5:
+                    printf("Five ");
+                case 6:
+                    printf("Six ");
+                    break;
+                case 7:
+                    printf("Seven ");
+                    break;
+                case 8:
+                    printf("Eight ");
+                    break;
+                case 9:
+                    printf("Nine ");
+                    break;
+                default:
+                    printf("Invalid entry\n");
+                    break;
+            }
         }
+        else
+            break;
     }
+
     
+    }
+
     putchar('\n');
 
     return 0;
 }
 
-    
