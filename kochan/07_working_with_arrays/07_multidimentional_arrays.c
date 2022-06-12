b664fafc45a245188344cc5cbdf8c9615e567e6a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-12 02:40:04 +0000

    Sun, Jun 12, 2022, 2:40 AM +00:00

---
+++ kochan/07_working_with_arrays/07_multidimentional_arrays.c
@@ -0,0 +1,27 @@
+/**
+ * 07_multidimentional_arrays.c
+ *
+ * Saturday, June 11 2022 22:35hrs
+ *
+ * This program illustrates how a multidimentional
+ * array is declared and initialized in C.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    /**
+     * Declares a two-dimentional array
+     * with two rows and three columes.
+     */
+    int values[2][3] =
+    {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    // Prints 6
+    printf("%i\n", values[1][2]);
+
+    return 0;
+}
