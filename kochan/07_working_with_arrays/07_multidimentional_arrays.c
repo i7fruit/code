7d8a499c1fb0eceb5db9ee7ccf95695f45a8471c
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-12 02:49:11 +0000

    Sun, Jun 12, 2022, 2:49 AM +00:00

--- kochan/07_working_with_arrays/07_multidimentional_arrays.c
+++ kochan/07_working_with_arrays/07_multidimentional_arrays.c
@@ -26,10 +26,13 @@ int main(void)
     // Three-dimentional array
     int numbers[3][2][4] =
     {
-        {},
-        {},
-        {}
+        {{1, 2, 3, 4}, {5, 6, 7, 8}},
+        {{9, 10, 11, 12}, {13, 14, 15, 16}},
+        {{17, 18, 19, 20}, {21, 22, 23, 24}}
     };
+
+    // Prints 13
+    printf("%i\n", numbers[1][1][0]);
 
     return 0;
 }
