a719ce416ffc1fbc8b8e0230a434ee4f465b0e6f
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-06-28 05:01:20 +0000

    Tue, Jun 28, 2022, 5:01 AM +00:00

---
+++ kochan/08_working_with_functions/10_global_variables.c
@@ -0,0 +1,70 @@
+/** 
+ * 10_global_variables.c
+ * 
+ * Tuesday, June 28 2022 00:31hrs
+ * 
+ * This program converts a number
+ * to a base specified by the user.
+ * It illustrates the use of global variables.
+ */
+#include <stdio.h>
+
+// Global variables - Global variable are initialized to zero by default
+int converted[64]; // Stores the converted number
+
+// Variables for base and number
+int base, index;
+unsigned long number;
+
+// Function prototypes
+void get_base_and_number(void);
+void convert_number(void);
+
+int main(void)
+{
+    get_base_and_number();
+    
+    printf("%%i, i, %lu, %i\n", index, base, number, converted[2]);
+
+    return 0;
+}
+
+/********************************
+ * get_base_and_number() prompts the
+ * user to enter values for base and
+ * number which alters the values of
+ * the corresponding global variables.
+ */
+void get_base_and_number(void)
+{
+    do
+    {
+        printf("Enter a base between 2 and 16: ");
+        scanf("%i", &base);
+    }
+    while (base < 2 || base > 16);
+
+    do
+    {
+        printf("Enter a positive integer: ");
+        scanf("%lu", &number);
+    }
+    while (number < 0);
+}
+
+/********************************************
+ * The convert_number() function converts the
+ * number entered by the user to a user-defined
+ * base using the modulo and division operators,
+ * and stores the number in the converted[] array
+ * in reverse order.
+ */
+void convert_number(void)
+{
+    while (number != 0)
+    {
+        converted[index] = number % base;
+        number /= base;
+        index++;
+    }
+}
