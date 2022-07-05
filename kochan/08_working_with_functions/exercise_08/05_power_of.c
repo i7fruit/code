ede4510e8cef7776e77d91808638ffe51f612fef
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-07-05 04:54:30 +0000

    Tue, Jul 5, 2022, 4:54 AM +00:00

---
+++ kochan/08_working_with_functions/exercise_08/05_power_of.c
@@ -0,0 +1,50 @@
+/** 
+ * 05_power_of.c
+ * 
+ * Tuesday, July 05 2022 00:42hrs
+ * 
+ * This program computes the value
+ * of an integer raised to the power
+ * of another integer.
+ */
+#include <stdio.h>
+
+// Function prototype
+int get_integer(void);
+long int power(int x, int y);
+
+int main(void)
+{
+    printf("Enter number: ");
+    int number = get_integer();
+    
+    printf("Enter power: ");
+    int power = get_integer();
+
+    printf("%i^%i = %li\n", number, power, power(number, power));
+
+    return 0;
+}
+
+/*******************************
+ * get_integer()
+ */
+int get_integer(void)
+{
+    int number = 0;
+
+    scanf("%i", &number);
+
+    return number;
+}
+
+/*******************************
+ * power()
+ */
+long int power(int x, int y)
+{
+    for (short i = 0; i < y; i++)
+        x *= x;
+    
+    return x;
+}
