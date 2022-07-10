d0dcfe1e381527ef2fd058db28ac00ca125f25a1
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-07-10 02:04:55 +0000

    Sun, Jul 10, 2022, 2:04 AM +00:00

---
+++ kochan/08_working_with_functions/exercise_08/07_least_common_multiple.c
@@ -0,0 +1,20 @@
+/**
+ * 07_least_common_multiple.c
+ *
+ * Saturday, July 09, 2022 22:00hrs
+ *
+ * The least common multiple of two positive
+ * numbers is the smallest number that can be
+ * evenly divided by the two numbers.
+ *
+ * The lcm of u and v can be computed by dividing the
+ * product of u * v by the greatest common divisor of u and v
+ *
+ * lcm = uv / gcd(u,v)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    int u, v;
+}
