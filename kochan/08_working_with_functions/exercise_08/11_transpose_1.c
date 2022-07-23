725aea87f89e5595458c1090f447a34eb507d84a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-07-23 23:18:50 +0000

    Sat, Jul 23, 2022, 11:18 PM +00:00

--- kochan/08_working_with_functions/exercise_08/11_transpose_1.c
+++ kochan/08_working_with_functions/exercise_08/11_transpose_1.c
@@ -1,8 +1,8 @@
-/**
+/** 
  * 11_transpose_1.c
- *
+ * 
  * Friday, July 22 2022 21:29hrs
- *
+ * 
  * This program takes a user-defined m by n matrix,
  * and transposes it.
  */
@@ -13,7 +13,7 @@ int get_integer(void);
 void fill_array(int r, int c, int array[][c]);
 void print_array(int r, int c, int array[][c]);
 void zero_out_array(int r, int c, int array[][c]);
-void transpose_matrix(int r, int c, int array0[][c], int array1[][r]);
+void transpose_matrix(int r, int c, int array0[r][c], int array1[c][r]);
 
 int main(void)
 {
@@ -31,6 +31,9 @@ int main(void)
 
     print_array(rows, col, values);
 
+    // For output readability
+    putchar('\n');
+
     // Variable to store transposed data
     int transposed_matrix[col][rows];
 
@@ -38,7 +41,8 @@ int main(void)
 
     transpose_matrix(rows, col, values, transposed_matrix);
 
-    print_array(rows, col, transposed_matrix);
+    // Switch values of col and rows to match rows and columes of the transposed matrix
+    print_array(col, rows, transposed_matrix);
 
     return 0;
 }
@@ -83,8 +87,6 @@ void print_array(int r, int c, int array[][c])
 
         putchar('\n');
     }
-
-    putchar('\n');
 }
 
 /************************************
@@ -101,13 +103,9 @@ void zero_out_array(int r, int c, int array[][c])
 /*************************************
  * transpose_matrix()
  */
-void transpose_matrix(int r, int c, int array0[][c], int array1[][r])
+void transpose_matrix(int r, int c, int array0[r][c], int array1[c][r])
 {
     for (short i = 0; i < r; i++)
-    {
         for (short j = 0; j < c; j++)
-        {
             array1[j][i] = array0[i][j];
-        }
-    }
 }
