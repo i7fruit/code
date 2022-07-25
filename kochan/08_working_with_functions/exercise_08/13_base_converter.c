492c0a1943e5d75a50f03d86947b12a7b626ddbf
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-07-25 04:29:16 +0000

    Mon, Jul 25, 2022, 4:29 AM +00:00

--- kochan/08_working_with_functions/exercise_08/13_base_converter.c
+++ kochan/08_working_with_functions/exercise_08/13_base_converter.c
@@ -9,18 +9,17 @@
  * be converted.
  */
 #include <stdio.h>
-#include <stdbool.h>
+
+// Function prototype
+void converter(unsigned long value, int base);
 
 int main(void)
 {
-    char base_digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-    int base = 0, size = 64;
+    int base = 0;
     unsigned long number = 0;
-    int converted[size];
-    bool is_converting = true;
 
-    while (is_converting)
+    // Continually prompts user for number to convert until zero is entered
+    while (1)
     {
         do
         {
@@ -30,29 +29,46 @@ int main(void)
             printf("Base number: ");
             scanf("%i", &base);
         }
-        while (base < 2 && base > 16);
+        while (base < 2 || base > 16);
 
+        // Stops prompting for numbers when user types in a zero to convert
         if (number == 0)
         {
-            printf("0\n");
+            printf("0\nEnd of conversion\n");
             break;
         }
 
-        static int i = 0;
-
-        while (number != 0)
-        {
-            converted[i++] = number % base;
-            number /= base;
-        }
-
-        for (--i; i > 0; i--)
-        {
-            printf("%c", base_digits[converted[i]]);
-        }
-        putchar('\n');
-        
+        converter(number, base);
     }
     
     return 0;
+}
+
+/***********************************
+ * converter()
+ */
+void converter(unsigned long value, int base)
+{
+    // Array to store digits for base characters
+    char base_digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    const short size = 64;
+    int converted[size];
+
+    // Initializes the converted[] array with zeros
+    for (short i = 0; i < size; i++)
+        converted[i] = 0;
+
+    int i = 0;
+
+    while (value != 0)
+    {
+        converted[i++] = value % base;
+        value /= base;
+    }
+
+    for (--i; i >= 0; i--)
+        printf("%c", base_digits[converted[i]]);
+    
+    putchar('\n');    
 }
