5e8cba5b57b5100d046b67afbf58251fe57d9edc
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-08-18 02:36:25 +0000

    Thu, Aug 18, 2022, 2:36 AM +00:00

--- kochan/10_character_strings/07_null_string.c
+++ kochan/10_character_strings/07_null_string.c
@@ -11,6 +11,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void readline(char arr[], int limit);
+
 int main(void)
 {
     const short size = 64;
@@ -29,4 +31,25 @@ int main(void)
     }
 
     count_words()
+}
+
+/****************************************************
+ * The readline() function takes a character array
+ * to store the string captured from standard input,
+ * and a second argument of an integer value representing
+ * the number of characters the user is limited to storing
+ * in the array. This way the user does not exceed the buffer.
+ */
+void readline(char arr[], int limit)
+{
+    short i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n' && i != limit);
+
+    // Overwrites '\n' with '\0'
+    arr[i] = '\0';
 }
