ebb100163ed3d16ef1e0397d0fd839a702bcf58d
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-08-18 02:19:24 +0000

    Thu, Aug 18, 2022, 2:19 AM +00:00

---
+++ kochan/10_character_strings/07_null_string.c
@@ -0,0 +1,21 @@
+/**
+ * 07_null_string.c
+ *
+ * Wednesday, August 17 2022 22:15hrs
+ *
+ * This program counts the number of words in a string
+ * of text that spans several lines. The user taps the
+ * Return/Enter key twice to indicat to the program that
+ * data entry has ended.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    const short size = 64;
+
+    // Array to store string of text entered by the user
+    char text[size] = "";
+
+    while
+}
