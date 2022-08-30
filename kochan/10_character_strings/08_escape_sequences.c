a6ab2d863fffe05501d764fae7870bdcc5343089
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-08-30 23:54:54 +0000

    Tue, Aug 30, 2022, 11:54 PM +00:00

---
+++ kochan/10_character_strings/08_escape_sequences.c
@@ -0,0 +1,20 @@
+/** 
+ * 08_escape_sequences.c
+ * 
+ * Tuesday, August 30 2022 18:09hrs
+ * 
+ * This program illustrates the use of
+ * some escape characters in C.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    // \" prints quotation marks "
+    printf("\"Hello\"\n");
+
+    // \? prints a question mark ?
+    printf("What's your name?\n");
+
+    return 0;
+}
