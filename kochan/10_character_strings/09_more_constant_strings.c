3349ceee2164c71672fbcb4ea697e0097c06747e
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-09-06 03:36:06 +0000

    Tue, Sep 6, 2022, 3:36 AM +00:00

---
+++ kochan/10_character_strings/09_more_constant_strings.c
@@ -0,0 +1,19 @@
+/**
+ * 09_more_constant_strings.c
+ *
+ * Monday, September 05, 2022 23:31hrs
+ *
+ * More on constant strings.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    // The backslash character can be used to continue string on a separate line
+    printf("If this is too long, \
+continue on a new line.\n");
+
+    // 
+
+    return 0;
+}
