fa83fe7d53c724beac37c578544778074a656d7e
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-09-08 02:22:46 +0000

    Thu, Sep 8, 2022, 2:22 AM +00:00

---
+++ kochan/10_character_strings/10_dictionary_0.c
@@ -0,0 +1,25 @@
+/**
+ * 10_dictionary_0.c
+ *
+ * Wednesday, September 07 2022 22:18hrs
+ *
+ * This program searches through an array
+ * of structs looking for a specific word.
+ * When found, it returns an integer indicating
+ * what position in the array the word is found,
+ * or a -1 to indicate that the word is not located
+ * in the array.
+ */
+#include <stdio.h>
+
+// Structure definition
+struct entry
+{
+    char word[15];
+    char defin
+}
+
+int main(void)
+{
+    // Declares an array of struct entry structs
+}
