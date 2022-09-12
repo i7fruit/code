8e4ae1dc998f080c56f9fc83b697ee0dd07c853d
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-09-12 01:55:07 +0000

    Mon, Sep 12, 2022, 1:55 AM +00:00

---
+++ kochan/10_character_strings/12_string_to_int.c
@@ -0,0 +1,14 @@
+/**
+ * 12_string_to_int.c
+ *
+ * Sunday, September 11 2022 21:53hrs
+ *
+ * This program converts character digits
+ * into integers.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    char d
+}
