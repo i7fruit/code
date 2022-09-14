d62d70719fd2a7600317e36458e652c2b991792a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-09-14 16:49:01 +0000

    Wed, Sep 14, 2022, 4:49 PM +00:00

---
+++ kochan/10_character_strings/exercise_10/01_substring.c
@@ -0,0 +1,69 @@
+/**
+ * 01_substring.c
+ *
+ * Wednesday, September 14 2022 12:31hrs
+ *
+ * This program extracts a substring from
+ * another string.
+ */
+#include <stdio.h>
+
+void get_text(char arr[]);
+void substring(const char source[], const int start, const int count, char result[]);
+
+int main(void)
+{
+    const int size = 64;
+    int low = 0, length = 0;
+
+    char string[size] = "";
+    char extract[size] = "";
+    char result[size] = "";
+
+    printf("Enter text: ");
+    get_text(string);
+
+    printf("Enter text to extract: ");
+    get_text(extract);
+
+    printf("Starting index: ");
+    scanf("%i", &low);
+
+    printf("Lenth of string: ");
+    scanf("%i", &length);
+
+    substring(string, low, length, result)
+
+
+    return 0;
+}
+
+/*****************************************
+ * The get_text() function prompts the user
+ * for a string of text using the getchar()
+ * function.
+ */
+void get_text(char arr[])
+{
+    int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    arr[i] = '\0';
+}
+
+/*****************************************
+ * The substring() function takes an array
+ * of characters and extracts a string of
+ * text from it starting from the starting
+ * index defined by the user. It saves the
+ * extracted text in another array.
+ */
+void substring(const char source[], const int start, const int count, char result[])
+{
+    for
+}
