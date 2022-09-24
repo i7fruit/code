fd6c56967b10686ed770a4c9682c50907ab6fa04
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-09-24 23:28:03 +0000

    Sat, Sep 24, 2022, 11:28 PM +00:00

--- kochan/10_character_strings/exercise_10/02_findstring.c
+++ kochan/10_character_strings/exercise_10/02_findstring.c
@@ -12,24 +12,59 @@
  */
 #include <stdio.h>
 
+// Function prototypes
+void get_text(char arr[]);
+
 int main(void)
 {
     const int size = 64;
 
     // Stores string of text to search
     char string[size] = "";
-    
+
     // Store string of text to find in the first SOT
     char word[size] = "";
 
     printf("Enter a string of text\n");
     get_text(string);
+
+    printf("Enter text to find: ");
+    get_text(word);
+
+    int index = find_string(string, word);
+
+    if (index != -1)
+        printf("Text exists at index %i.\n", index);
+
+    else
+        printf("Text does not exist.\n");
+
+    return 0;
 }
 
-/************************************************
- * get_text()
+/*****************************************
+ * The get_text() function prompts the user
+ * for a string of text using the getchar()
+ * function.
  */
-void get_text(const char arr[])
+void get_text(char arr[])
 {
     int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    arr[i] = '\0';
+}
+
+/******************************************************
+ * find_string()
+ */
+int find_string(const char arr_0[], const char arr_1[])
+{
+    // Computes length of each string
+    int 
 }
