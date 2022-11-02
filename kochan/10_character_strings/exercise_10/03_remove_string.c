5c662a3e58a4e668e426cd7234679eab50bb481a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-11-02 20:31:19 +0000

    Wed, Nov 2, 2022, 8:31 PM +00:00

---
+++ kochan/10_character_strings/exercise_10/03_remove_string.c
@@ -0,0 +1,51 @@
+/**
+ * 03_remove_string.c
+ *
+ * Wednesday, November 02 2022 16:15hrs
+ *
+ * This function extracts a string of text from
+ * another and prints to standard output the contents
+ * of the array where the string of text was extracted from.
+ */
+#include <stdio.h>
+
+// Function prototypes
+void get_text(char arr[]);
+
+int main(void)
+{
+    const short size = 64;
+
+    // Stores string of text
+    char text[size] = "";
+
+    // Stores starting index of extraction, and number of characters to extract
+    int start = 0, number_of_characters = 0;
+
+    printf("Enter a string of text below\n");
+    get_text(text);
+
+    printf("With the first character position being 0, what position in the text do \
+    you want to start the extraction: ");
+    scanf("%i", &start);
+
+    printf("How many characters do you want to extract? ");
+    scanf("%i", &number_of_characters);
+
+    return 0;
+}
+
+// ****************************************************************
+void get_text(char arr[])
+{
+    int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    // Overwrites the newline character with a null zero
+    arr[i] = '\0';
+}
