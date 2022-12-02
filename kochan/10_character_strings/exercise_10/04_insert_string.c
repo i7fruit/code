18f7c36716adb58af07439f2f7b41bb752148b29
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-12-02 03:00:32 +0000

    Fri, Dec 2, 2022, 3:00 AM +00:00

---
+++ kochan/10_character_strings/exercise_10/04_insert_string.c
@@ -0,0 +1,54 @@
+/**
+ * 04_insert_string.c
+ *
+ * Thursday, December 01, 2022 21:48hrs
+ *
+ * This program inserts a string of text into
+ * another string of text and displays the result
+ * to standard output.
+ */
+#include <stdio.h>
+
+// Function prototypes
+void get_text(char arr[]);
+
+int main(void)
+{
+    // Maximum size of array
+    const int size = 64;
+
+    // Index to start insertion from
+    int insertion_index = 0;
+
+    // String of text to insert text into
+    char text[size] = "";
+
+    // String of text to insert
+    char string[size] = "";
+
+    printf("Enter a string of text: ");
+    get_text(text);
+
+    printf("%s\n", text);
+
+    return 0;
+}
+
+/***********************************************
+ * The get_text() function uses the getchar() function
+ * to get a string of text from the user and stores
+ * it in an array using a do-while loop
+ */
+void get_text(char arr[])
+{
+    int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    // Overwrites newline character with an ascii 0
+    arr[i] = '\0';
+}
