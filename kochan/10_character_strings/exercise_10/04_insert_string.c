e6dec66a2835fc8cae078f2b91e195cafc593fd6
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-12-12 01:50:11 +0000

    Mon, Dec 12, 2022, 1:50 AM +00:00

--- kochan/10_character_strings/exercise_10/04_insert_string.c
+++ kochan/10_character_strings/exercise_10/04_insert_string.c
@@ -66,13 +66,20 @@ void get_text(char arr[])
  */
 void insert_string(char arr_0[], char arr_1[], int element)
 {
-    // Array to store full text
-    int a = strlen(arr_0), b = strlen(arr_1), c;
+    // Counts number of characters from insertion point
+    int count = 0, i = -1;
 
-    int c = a + b;
-    char concatenated[c] = "";
+    while (arr_0[++i] != '\0')
+        count++;
 
-    int i = 0, j = 0;
+    // Saves those chars in a temporary array
+    char temp[count] = "";
+
+    for (int i = element, j = 0; arr_0[i] != '\0'; i++, j++)
+        temp[j] = arr_0[i];
+
+
+
 
     // Inserts characters before insertion
     for (; i < element; i++)
