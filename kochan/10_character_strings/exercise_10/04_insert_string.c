94e452a4e3cbad21a478605402cbb68689e41264
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2022-12-12 02:29:11 +0000

    Mon, Dec 12, 2022, 2:29 AM +00:00

--- kochan/10_character_strings/exercise_10/04_insert_string.c
+++ kochan/10_character_strings/exercise_10/04_insert_string.c
@@ -41,7 +41,7 @@ int main(void)
     int a = strlen(text), b = strlen(string), c;
 
     c = a + b;
-    char concatenated[c] = "";
+    char concatenated[c];
 
     insert_string(text, string, insertion_index);
 
