606784494476001d66721596a7486130c6df5053
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-01-07 04:49:58 +0000

    Sat, Jan 7, 2023, 4:49 AM +00:00

--- kochan/10_character_strings/exercise_10/05_replace_string.c
+++ kochan/10_character_strings/exercise_10/05_replace_string.c
@@ -146,10 +146,6 @@ void remove_string(char arr[], int index, const int count)
     int end = (index + count) - 1;
     int i = index;
 
-    // Ends program if word to extract extends beyond end of original array
-    if (end >= size)
-        exit(1);
-
     /**
      * Traverses array from the starting index (i) of the substring
      * to the end of the array (size). Continues as long as the array
