ebfcc65eae5144a9dd3dc5904cb8bc0be86b7c7b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-01-10 02:46:03 +0000

    Tue, Jan 10, 2023, 2:46 AM +00:00

--- kochan/10_character_strings/exercise_10/05_replace_string.c
+++ kochan/10_character_strings/exercise_10/05_replace_string.c
@@ -96,6 +96,8 @@ void replace_string(char source[], char remove[], char insert[])
     int temp_array_size = strlen(temp);
     int limit = insert_array_size + temp_array_size;
 
+    int j = 0;
+    
     for (i = insert_array_size, j = 0; i < limit; i++, j++)
         insert[i] = temp[j];
 
