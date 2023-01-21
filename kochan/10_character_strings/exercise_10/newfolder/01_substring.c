17458428206070978a15d33b16c3b3725c1c61b4
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-01-21 05:22:13 +0000

    Sat, Jan 21, 2023, 5:22 AM +00:00

---
+++ kochan/10_character_strings/exercise_10/newfolder/01_substring.c
@@ -0,0 +1,78 @@
+/**
+ * 01_substring.c
+ *
+ * Wednesday, September 14 2022 12:31hrs
+ *
+ * This program extracts a substring from
+ * another string.
+ */
+#include <stdio.h>
+#include <string.h>
+
+void get_text(char arr[]);
+void substring(const char source[], const int start, const int count, char result[]);
+
+int main(void)
+{
+    const int size = 64;
+    int low = 0, length = 0;
+
+    char string[size] = "";
+    char result[size] = "";
+
+    printf("Enter text: ");
+    get_text(string);
+
+    printf("Starting index: ");
+    scanf("%i", &low);
+
+    printf("Lenth of string: ");
+    scanf("%i", &length);
+
+    substring(string, low, length, result);
+
+    printf("%s\n", result);
+
+    return 0;
+}
+
+/*****************************************
+ * The get_text() function prompts the user
+ * for a string of text using the getchar()
+ * function.
+ */
+void get_text(char arr[])
+{
+    int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    arr[i] = '\0';
+}
+
+/*****************************************
+ * The substring() function takes an array
+ * of characters and extracts a string of
+ * text from it starting from the starting
+ * index defined by the user. It saves the
+ * extracted text in another array.
+ */
+void substring(const char source[], const int start, const int count, char result[])
+{
+    int end = strlen(source);
+
+    if (start < end)
+    {
+        for (int i = start, j = 0; source[i] != '\0'; i++, j++)
+        {
+            result[j] = source[i];
+        }
+    }
+
+    else
+        printf("No text to extract.");
+}
---
+++ kochan/10_character_strings/exercise_10/newfolder/03_remove_string.c
@@ -0,0 +1,86 @@
+/**
+ * 03_remove_string.c
+ *
+ * Wednesday, November 02 2022 16:15hrs
+ *
+ * This function extracts a string of text from
+ * another and prints to standard output the contents
+ * of the array where the string of text was extracted from.
+ */
+#include <stdio.h>
+#include <string.h>
+
+// Function prototypes
+void get_text(char arr[]);
+void remove_string(char arr[], int index, int count);
+
+int main(void)
+{
+    const short size = 64;
+
+    // Stores string of text
+    char text[size] = "";
+
+    // Stores starting index of extraction, and number of characters to extract
+    int start = 0, number_of_characters = 0;
+
+    printf("Enter a string of text below\n");
+    get_text(text);
+
+    printf("With the first character position being 0, what position in the text do \
+you want to start the extraction: ");
+    scanf("%i", &start);
+
+    printf("How many characters do you want to extract? ");
+    scanf("%i", &number_of_characters);
+
+    remove_string(text, start, number_of_characters);
+
+    printf("%s\n", text);
+
+    return 0;
+}
+
+/*************************************************************************/
+void get_text(char arr[])
+{
+    int i = -1;
+
+    do
+    {
+        arr[++i] = getchar();
+    }
+    while (arr[i] != '\n');
+
+    // Overwrites the newline character with a null zero
+    arr[i] = '\0';
+}
+
+/***************************************************************************
+ * The remove_string() function takes as arguments an array that stores a string
+ * of text, and two integers that store values for the starting index of a string
+ * to be extracted from the array, and the number of characters that string will contain.
+*/
+void remove_string(char arr[], int index, int count)
+{
+    // Computes length of string to extract from
+    int size = strlen(arr);
+    int end = index + count;
+    int i = index;
+
+    /**
+     * Traverses array from the starting index (i) of the substring
+     * to the end of the array (size). Continues as long as the array
+     * is traversible, and the end of the substring (end), is within
+     * the bounds of the array (size)
+     */
+    for (; i < size && end < size; i++, end++)
+    {
+        // Overwrites characters to be removed with characters following removed characters
+        arr[i] = arr[end];
+    }
+
+    // Terminates the rest of the array when the end of the substring is reached
+    if (i < size)
+        arr[i] = '\0';
+}
