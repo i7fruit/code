a6c37f16cf4a8621a87a550121bde06f25668c4d
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-01-21 23:06:15 +0000

    Sat, Jan 21, 2023, 11:06 PM +00:00

--- kochan/10_character_strings/exercise_10/06_dictionary_sort.c
+++ kochan/10_character_strings/exercise_10/06_dictionary_sort.c
@@ -9,10 +9,32 @@
 #include <stdio.h>
 
 #define FIFTEEN 15
-#define SIXTY
+#define SIXTY 60
+#define SIZE 12
 
 // Structure definition
 struct entry
 {
     char word[FIFTEEN];
+    char definition[SIXTY];
+};
+
+int main(void)
+{
+    // Declares an array of struct entry structure variables
+    const struct entry dictionary[SIZE] =
+    {
+        { "aardvark", "a burrowing African mammal" },
+        { "abyss", "a bottomless pit" },
+        { "acumen", "mentally sharp; keen" },
+        { "babydoll", "a doll designed to look like a baby"},
+        { "addle", "to become confused" },
+        { "aerie", "a high nest" },
+        { "affix", "to append; attach" },
+        { "agar", "a jelly made from seaweed" },
+        { "ahoy", "a nautical call of greeting" },
+        { "aigrette", "an ornamental cluster of feathers" },
+        { "ajar", "partially opened" },
+        { "cagey", "reluctant to give information owing to caution or suspicion"}
+    };
 }
