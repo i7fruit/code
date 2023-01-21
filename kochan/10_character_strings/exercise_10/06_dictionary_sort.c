cbab16fcde248af47abed12eb54b84f08b8290d1
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-01-21 22:58:54 +0000

    Sat, Jan 21, 2023, 10:58 PM +00:00

---
+++ kochan/10_character_strings/exercise_10/06_dictionary_sort.c
@@ -0,0 +1,18 @@
+/**
+ * 06_dictionary_sort.c
+ *
+ * Saturday, January 21, 2023 17:56hrs
+ *
+ * This program uses bubble sort to sort
+ * a dictionary in ascending order.
+*/
+#include <stdio.h>
+
+#define FIFTEEN 15
+#define 
+
+// Structure definition
+struct entry
+{
+    char word[FIFTEEN];
+}
