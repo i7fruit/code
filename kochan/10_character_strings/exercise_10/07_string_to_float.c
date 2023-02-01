8a6f7606bd21b909c5744141c2c04bdf770d6a2f
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-01 02:40:54 +0000

    Tue, Jan 31, 2023, 9:40 PM -05:00

--- kochan/10_character_strings/exercise_10/07_string_to_float.c
+++ kochan/10_character_strings/exercise_10/07_string_to_float.c
@@ -7,55 +7,21 @@
  * into floating point numbers.
  */
 
-// Save pre-decimal digits in one array and post-decimal digits in another array.
-// Find lenght of second array and set it to power of ten and compute value
-// convert digits in second array to int, and divide by value computed from power of ten
-// Add value of both arrays
 #include <stdio.h>
-#include <ctype.h>
-#include <math.h>
 
 int main(void)
 {
-    char text[] = "12.98";
+    const int size = 64;
+    char text[size];
 
-    double pre_decimal = 0.0;
-    double post_decimal = 0.0;
+    // Prompts the user for a floating-point number
+    get_text(text);
 
-    int i = 0;
-
-    for (; text[i] != '\0'; i++)
-    {
-        if (text[i] == '.')
-        {
-            i++;
-            break;
-        }
-
-        int digit = text[i] - 48;
-
-        pre_decimal = pre_decimal * 10 + digit;
-    }
-
-    static int count = 0;
-
-    for (; text[i] != '\0'; i++)
-    {
-
-        double digit = text[i] - 48;
-
-        post_decimal = (post_decimal * 10.0 + digit) * 1.0;
-
-        count++;
-    }
-
-    int power = pow(10, count);
-
-    post_decimal /= power;
-
-    double value = pre_decimal + post_decimal;
-
-    printf("%.f\n", value);
+    printf("%s\n", text);
 
     return 0;
 }
+
+/*********************************************
+ * get_text()
+*/
