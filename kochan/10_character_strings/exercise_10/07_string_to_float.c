91395ae3c63a4c3736e27b7745f6cf27d9a5897f
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-10 23:19:31 +0000

    Fri, Feb 10, 2023, 6:19 PM -05:00

--- kochan/10_character_strings/exercise_10/07_string_to_float.c
+++ kochan/10_character_strings/exercise_10/07_string_to_float.c
@@ -28,9 +28,11 @@ int main(void)
 
     float converted = 0;
 
-    converted = string_to_float(text);
+    //converted = string_to_float(text);
 
-    printf("%.10f\n", converted);
+    //printf("%.10f\n", converted);
+
+    printf("%s\n", text);
 
     return 0;
 }
