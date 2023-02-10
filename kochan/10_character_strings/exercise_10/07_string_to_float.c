729fc1917aed0094918910dda5ae8d433356e38e
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-10 23:42:38 +0000

    Fri, Feb 10, 2023, 6:42 PM -05:00

--- kochan/10_character_strings/exercise_10/07_string_to_float.c
+++ kochan/10_character_strings/exercise_10/07_string_to_float.c
@@ -91,7 +91,7 @@ void get_text(char arr[])
             }
 
             // Stores a minus sign if encountered for the first time
-            if (arr[j] == '-' && !is_digit && !is_negative && !found_decimal)
+            if (arr[j] == '-' && !is_digit && !is_negative)
             {
                 is_negative = 1;
                 temp[i] = arr[j];
