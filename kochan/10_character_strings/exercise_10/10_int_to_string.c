754e57f0dd7ed142b85951c042bccfe16ed6d3e4
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-11 20:50:06 +0000

    Sat, Feb 11, 2023, 3:50 PM -05:00

--- kochan/10_character_strings/exercise_10/10_int_to_string.c
+++ kochan/10_character_strings/exercise_10/10_int_to_string.c
@@ -3,5 +3,12 @@
  *
  * Saturday, February 11, 2023 15:48hrs
  *
- * 
+ * This program takes an integer input from
+ * the user and converts it to a string.
 */
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
