72763fa637827969982bf61836a58a703634e66a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-11 22:57:34 +0000

    Sat, Feb 11, 2023, 5:57 PM -05:00

--- kochan/10_character_strings/exercise_10/10_int_to_string.c
+++ kochan/10_character_strings/exercise_10/10_int_to_string.c
@@ -40,7 +40,7 @@ long long get_integer(void)
         printf("Enter an integer: ");
         scanf("%lli", &i);
     }
-    while (i < 0);
+    while (i < -18000000000000000000 || i > 18000000000000000000);
 
     return i;
 }
