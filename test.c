4a42c3b9e6690e3171618448ff13ea500d0c2a1a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-14 03:24:37 +0000

    Mon, Feb 13, 2023, 10:24 PM -05:00

--- test.c
+++ test.c
@@ -2,7 +2,26 @@
 
 int main(void)
 {
-    char number[] = "90.3";
+    int arr[] = {1, 4, 3, 2, 5};
 
-    printf("%.f\n", (float)number);
+    for (int i = 0, j = 1; i < 5; i++)
+    {
+        int min = i;
+
+        for (; j < 5; j++)
+        {
+            if (arr[min] > arr[j])
+                min = j;
+        }
+
+        if (min != i)
+        {
+            int temp = arr[min];
+            arr[min] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    for (int k = 0; k < 5; k++)
+        printf("%i ", arr[k]);
 }
