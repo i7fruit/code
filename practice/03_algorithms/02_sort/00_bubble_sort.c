e0f7080b104950083a64dbb181ecdd53e65a7c3a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-15 03:22:14 +0000

    Tue, Feb 14, 2023, 10:22 PM -05:00

--- practice/03_algorithms/02_sort/00_bubble_sort.c
+++ practice/03_algorithms/02_sort/00_bubble_sort.c
@@ -26,9 +26,12 @@ int main(void)
 
         for (int i = 0, j = 1; i < size - 1; i++)
         {
-            for (int j = 1; j < size; j++)
+            if (arr[j - 1] > arr[j])
             {
-                
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] =  temp;
+                counter++;
             }
         }
     }
