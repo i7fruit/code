7eecc7dfe8637bd62e3b7cdb7c6740362919a314
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-15 00:07:00 +0000

    Tue, Feb 14, 2023, 7:07 PM -05:00

--- practice/03_algorithms/02_sort/01_selection_sort.c
+++ practice/03_algorithms/02_sort/01_selection_sort.c
@@ -2,20 +2,27 @@
 
 int main(void)
 {
+    // Array size
     int size = 7;
 
     int arr[] = {1, 129, 4, 3, 2, 5, 0};
 
+    // Loops through the array
     for (int i = 0; i < size; i++)
     {
+        // Sets array element with assumed smallest value
         int min = i;
 
+        // Loops though the elements comparing their values with the assumed smallest
         for (int j = i + 1; j < size; j++)
         {
+            // Sets a new element as that with the smallest value if new smallest value found in said element
             if (arr[min] > arr[j])
                 min = j;
         }
 
+        /**
+         *  If after iterating through array, smallest value is
         if (min != i)
         {
             int temp = arr[min];
