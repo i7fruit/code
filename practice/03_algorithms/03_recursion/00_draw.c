f5bd05bfaa94f8ec2a1a8552baa13c0ad0a84443
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-17 14:41:32 +0000

    Fri, Feb 17, 2023, 9:41 AM -05:00

---
+++ practice/03_algorithms/03_recursion/00_draw.c
@@ -0,0 +1,34 @@
+/**
+ * 00_draw.c
+ *
+ * Friday, February 17 2023, 09:37hrs
+ *
+ * This program prints a triangle using
+ * a recursive function
+*/
+#include <stdio.h>
+
+// Function prototype
+void draw(int i);
+
+int main(void)
+{
+    draw(3);
+}
+
+/********************************************
+ * draw()
+*/
+void draw(int i)
+{
+    if (i == 1)
+        return;
+
+    else
+    {
+        for (int j = 0; j < i; i++)
+        {
+            draw()
+        }
+    }
+}
