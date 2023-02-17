25c8bdf6dc8c5e534f9ab56488a0e467c1e81a6b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-17 18:11:50 +0000

    Fri, Feb 17, 2023, 1:11 PM -05:00

--- practice/03_algorithms/03_recursion/00_draw.c
+++ practice/03_algorithms/03_recursion/00_draw.c
@@ -9,11 +9,31 @@
 #include <stdio.h>
 
 // Function prototype
+int get_integer(char arr[]);
 void draw(int i);
 
 int main(void)
 {
-    draw(3);
+    int size = get_integer("Enter size between 1 and 20: ");
+
+    draw(size);
+
+    return 0;
+}
+
+/****************************************
+ * get_integer() prompts the user for an integer
+*/
+int get_integer(char arr[])
+{
+    int i = 0;
+
+    do
+    {
+        printf("%s\n", arr);
+        scanf("%i", &i);
+    }
+    while (i < 1 || i > 20);
 }
 
 /********************************************
