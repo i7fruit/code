690cfa125c30fae298c44afd3cd6c91d823ed2a4
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-18 23:48:48 +0000

    Sat, Feb 18, 2023, 6:48 PM -05:00

---
+++ practice/03_algorithms/02_sort/04_merge_sort.c
@@ -0,0 +1,12 @@
+/**
+ * 04_merge_sort.c
+ *
+ * Saturday, February 18 2023 18:47hrs
+ *
+ * This program sorts an array of integers
+ * by recursively splitting the array and
+ * sorting its resulting subarrays until
+ * each subarray contains only one element,
+ * after which it merges them in ascending
+ * or decending order
+*/
