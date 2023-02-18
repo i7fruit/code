34040e86894ed1f167a89895a4337b38849d1566
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-18 04:36:44 +0000

    Fri, Feb 17, 2023, 11:36 PM -05:00

--- practice/03_algorithms/03_recursion/01_colatz.c
+++
@@ -1,8 +0,0 @@
-/**
- * 01_colatz.c
- *
- * Friday, February 17 2023 23:30hrs
- *
- * The Colatz conjecture computes the number
- * of steps it take
-*/
---
+++ practice/03_algorithms/03_recursion/01_factorial.c
@@ -0,0 +1,16 @@
+/**
+ * 01_factorial.c
+ *
+ * Friday, February 17 2023 23:30hrs
+ *
+ * The factorial of any number is the product
+ * of all the numbers from 1 up to that number.
+ * This program uses a recursive function to
+ * compute the factorial of any given number
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
