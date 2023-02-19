eba2be884378a39e2bc3cc363799151be4d9d3c7
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-19 01:11:58 +0000

    Sat, Feb 18, 2023, 8:11 PM -05:00

--- practice/03_algorithms/02_sort/04_merge_sort.c
+++ practice/03_algorithms/02_sort/04_merge_sort.c
@@ -79,10 +79,12 @@ void merge(int arr[], int lo, int mid, int hi)
     int right_subarray[size_right_subarray];
 
     // Initializes the subarrays
-    int i = 0;
-    for (; i < size_left_subarray; i++)
-        left_subarray[i] = arr[i];
+    int i = 0, j = 0, k = 0;
+    for (; i < size_left_subarray; i++, k++)
+        left_subarray[i] = arr[k];
 
-    for (; i < size_right_subarray; i++)
-        right_subarray[i] = arr[i];
+    for (; j < size_right_subarray; j++, k++)
+        right_subarray[j] = arr[k];
+
+    // Merges both
 }
