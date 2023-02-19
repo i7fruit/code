d94acae5110d068cba43429fd36404876bcf6a3a
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-02-19 00:15:22 +0000

    Sat, Feb 18, 2023, 7:15 PM -05:00

--- practice/03_algorithms/02_sort/04_merge_sort.c
+++ practice/03_algorithms/02_sort/04_merge_sort.c
@@ -22,8 +22,17 @@ int main(void)
     // Computes lowest and highest indices
     int lo = 0, hi = size - 1;
 
-    merge_sort(arr, lo, hi);
+    sort(arr, lo, hi);
 
     return 0;
 }
 
+/********************************************************
+ * This sort function takes an array of type int,
+ * and two integers. It first finds the midpoint
+ * of the array if its elements exceed one, then
+ * it calls itself to sort the corresponding subarrays.
+ * After the subarrays are sorted, it calls on the
+ * merge function to combine the subarrays into a
+ * fully sorted array.
+ */
