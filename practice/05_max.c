5200bfec4a1c6a3fd40acaad2cd0ce427c0e4cee
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-03 16:25:32 +0000

    Fri, Mar 3, 2023, 11:25 AM -05:00

--- practice/05_max.c
+++ practice/05_max.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
@@ -27,5 +27,11 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
-    return 0;
+    int max = 0;
+
+    for (int i = 0; i < n; i++)
+        if (array[i] > array[max])
+            max = i;
+
+    return array[max];
 }
