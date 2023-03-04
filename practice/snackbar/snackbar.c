cd92f3b2b031bb2869a4c7f07229bfe23c949a4e
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-04 01:47:05 +0000

    Fri, Mar 3, 2023, 8:47 PM -05:00

--- practice/snackbar/snackbar.c
+++ practice/snackbar/snackbar.c
@@ -75,19 +75,7 @@ int main(void)
 void add_items(void)
 {
 
-    menu =
-    {
-        {.item = "Burger: ", .price = 9.5}
-        //{"Vegan Burger: ", 11},
-        //{"Hot Dog: ", 5},
-        //{"Cheese Dog: ", 7},
-        //{"Fries: ", 5},
-        //{"Cheese Fries: ", 6},
-        //{"Cold Pressed Juice: ", 7},
-        //{"Cold Brew: ", 3},
-        //{"Water: ", 2},
-        //{"Soda: ", 2}
-    };
+    menu 
 }
 
     return;
