a87c9d62a1843b88cc777b5d7e07a1b644230fab
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-10 17:44:06 +0000

    Fri, Mar 10, 2023, 12:44 PM -05:00

---
+++ practice/06_pointers/03_swap.c
@@ -0,0 +1,37 @@
+/**
+ * 03_swap.c
+ *
+ * Friday, March 10 2023 12:39hrs
+ *
+ * Swaps the values of two variables
+ */
+#include <stdio.h>
+
+// Function prototype
+void swap(int &a, int &b);
+
+int main(void)
+{
+    int x = 1, y = 2;
+
+    printf("x = %i, y = %i\n", x, y);
+
+    swap(&x, &y);
+
+    printf("x = %i, y = %i\n", x, y);
+
+    return 0;
+}
+
+/**************************************
+ * swap()
+*/
+void swap(int &x, int &y)
+{
+    int temp = *x;
+
+    *x = *y;
+
+    *y = temp;
+
+}
