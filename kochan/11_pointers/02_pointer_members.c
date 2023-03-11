8948fe80f6b42b99cb16baac836d6db303b03c44
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-11 17:31:54 +0000

    Sat, Mar 11, 2023, 12:31 PM -05:00

---
+++ kochan/11_pointers/02_pointer_members.c
@@ -0,0 +1,15 @@
+/**
+ * 02_pointer_members.c
+ *
+ * Saturday, March 11 2023 12:29hrs
+ *
+ * This program illustrates the use of structure variables
+ * that have pointers as its members
+*/
+#include <stdio.h>
+
+// Structure definition
+typedef struct
+{
+    
+}
