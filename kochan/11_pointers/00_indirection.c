7b846705a610527480911560064f7685b31c5b76
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-11 02:58:13 +0000

    Fri, Mar 10, 2023, 9:58 PM -05:00

---
+++ kochan/11_pointers/00_indirection.c
@@ -0,0 +1,17 @@
+/**
+ * 00_indirection.c
+ *
+ * Friday, March 10 2023 21:51hrs
+ *
+ * This program illustrates the use
+ * of pointers
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int value = 10;
+
+    // Declares a pointer to the variable, value
+    int *ptr = &value;
+}
