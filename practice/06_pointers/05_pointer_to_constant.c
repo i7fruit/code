a59b62097af220d51ee27f3f0d418f56c1b5c634
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-15 01:56:53 +0000

    Tue, Mar 14, 2023, 9:56 PM -04:00

---
+++ practice/06_pointers/05_pointer_to_constant.c
@@ -0,0 +1,15 @@
+/**
+ * 05_pointer_to_constant.c
+ *
+ * Tuesday, March 14 2023 21:54hrs
+ *
+ * If the location will not change through
+ * the pointer variable, the pointer can be
+ * declared as pointing to a constant variable
+ */
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
