24cb92cfd257fbcdfe096c1e3deb1cebf7cc1a7b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-17 03:58:06 +0000

    Thu, Mar 16, 2023, 11:58 PM -04:00

---
+++ kochan/11_pointers/08_pointers_as_args.c
@@ -0,0 +1,20 @@
+/**
+ * 08_pointers_as_args.c
+ *
+ * Thursday, March 16 2023 23:54hrs
+ *
+ * This program illustrates the use of
+ * pointers as arguments to functions. The
+ * pointers are passed by value ie they are
+ * copied to the function so any changes to
+ * the argument does not affect the pointer
+ * that was passed to the function. However,
+ * the value that that pointer points to can
+ * be changed.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    short
+}
