b1afc4d5e60fa7f41a43fd1201a3028975972366
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-17 21:48:41 +0000

    Fri, Mar 17, 2023, 5:48 PM -04:00

---
+++ kochan/11_pointers/09_list_search.c
@@ -0,0 +1,23 @@
+/**
+ * 09_list_search.c
+ *
+ * Friday, March 17 2023 17:46hrs
+ *
+ * This program traverses through a linked list
+ * searching for a specific entry. Once the entry
+ * is found, it returns a pointer to that entry.
+*/
+#include <stdio.h>
+
+// Struct definition
+typedef struct entry
+{
+    int value;
+    struct entry *next;
+}
+entry;
+
+int main(void)
+{
+    
+}
