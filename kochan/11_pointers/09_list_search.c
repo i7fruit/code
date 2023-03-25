0c3147e3507661dd4b4a04cf6cff8e53981e3f37
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-25 19:34:43 +0000

    Sat, Mar 25, 2023, 3:34 PM -04:00

--- kochan/11_pointers/09_list_search.c
+++ kochan/11_pointers/09_list_search.c
@@ -8,6 +8,7 @@
  * is found, it returns a pointer to that entry.
 */
 #include <stdio.h>
+#include <limits.h>
 
 // Struct definition
 typedef struct entry
@@ -17,7 +18,43 @@ typedef struct entry
 }
 entry;
 
+// Function prototypes
+
+
 int main(void)
 {
-    
+    entry first, second, third;
+    first = (entry){100, &second};
+    second = (entry){200, &third};
+    third = (entry){300, (struct entry *) 0};
+
+    int number = get_integer("Number: ");
+
+    // Traverses through the linked list
+    entry *list_start = &first; // Points to the start of the list
+
+    while (list_start != (struct entry *) 0)
+    {
+        entry *ptr = list_start;
+        if (ptr->)
+    }
+}
+
+/*************************************************************
+ * The get_integer() function returns an integer entered by
+ * the user
+*/
+int get_integer(char arr[])
+{
+    printf("%s", arr);
+
+    int i = 0;
+
+    do
+    {
+        scanf("%i", &i);
+    }
+    while (i < 0 || i > INT_MAX);
+
+    return i;
 }
