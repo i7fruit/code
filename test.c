9730b8e23cc5444be7e2bd308c8a6b276f6c974b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-31 04:24:53 +0000

    Fri, Mar 31, 2023, 12:24 AM -04:00

--- test.c
+++ test.c
@@ -2,10 +2,11 @@
 
 int main() {
   char *filename = "collins.txt";
-  char *write = "w";
 
   // Create a file on your computer (filename.txt)
-  FILE *fptr = fopen(filename, write);
+  FILE *fptr = fopen(filename, "a");
+
+  fprintf(fptr, "I don't know. Find out.\n");
 
   // Close the file
   fclose(fptr);
