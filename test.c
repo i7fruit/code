3d61913df3193e70b0dc34fcc3a8e682fbfcb855
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-03-31 04:17:55 +0000

    Fri, Mar 31, 2023, 12:17 AM -04:00

--- test.c
+++ test.c
@@ -1,59 +1,14 @@
 #include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <stdlib.h>
 
-int get_int(char arr[]);
+int main() {
+  char *filename = "collins.txt";
+  char *write = 'w';
 
-int main(void)
-{
-    int *numbers = NULL;
-    int capacity = 0;
+  // Create a file on your computer (filename.txt)
+  FILE *fptr = fopen(filename, write);
 
+  // Close the file
+  fclose(fptr);
 
-    int size = 0;
-
-    while (true)
-    {
-        int number = get_int("Number: ");
-
-        if (number == INT_MAX)
-            break;
-
-        int *nu = realloc(numbers, sizeof(int) * (size + 1));
-
-        if (!nu)
-        {
-            fprintf(stderr, "Insufficient memory\n");
-            if (numbers)
-               free(numbers);
-            return 1;
-        }
-
-        numbers = nu;
-        capacity++;
-
-        numbers[size] = number;
-        size++;
-    }
-
-    for (int z = 0; z < size; z++)
-        printf("%i ", numbers[z]);
-
-    putchar('\n');
-
-    return 0;
-}
-
-int get_int(char arr[])
-{
-    printf("%s", arr);
-
-    int i = 0;
-    scanf("%i", &i);
-
-    if (feof(stdin))
-        return INT_MAX;
-
-    return i;
+  return 0;
 }
