04692a301c57fe5b20d49f22cbe38b0f669f5c53
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-03 20:52:20 +0000

    Mon, Apr 3, 2023, 4:52 PM -04:00

---
+++ practice/06_pointers/04_realloc.c
@@ -0,0 +1,25 @@
+/**
+ * 04_realloc.c
+ *
+ * Monday, April 03 2023 16:47hrs
+ *
+ * This program illustrates how an array can be
+ * dynamically initialized using realloc
+*/
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    // Pointer to memory location for array
+    int *values = NULL;
+    int size = 0;
+
+    // Prompts the user for input
+    while (true)
+    {
+        int number = get_int("Number: ");
+    }
+}
