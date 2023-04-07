8f47d3f4bea6e4b39c9bf1a4eddc9d42653a1650
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-07 04:26:09 +0000

    Fri, Apr 7, 2023, 12:26 AM -04:00

--- test.c
+++ test.c
@@ -1,85 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <limits.h>
 
-typedef struct point
+int main(void)
 {
-  int x;
-  struct point *next;
-}
-point;
+    printf("%i\n", INT_MAX);
+    printf("%li\n", LONG_MAX);
+    printf("%lli\n", LLONG_MAX);
 
-// Function prototypes
-int get_int(char arr[]);
-
-int main()
-{
-  point * mypoint = NULL;
-
-  /**
-   * Dynamically allocate a new point
-   * struct which mypoint points to here
-   */
-
-  while (true)
-  {
-      int number = get_int("Enter number: ");
-
-      if (number == INT_MAX)
-          break;
-
-      // Create space for new node
-      point *nu = (point *)malloc(sizeof(point));
-
-      if (!nu)
-      {
-          fprintf(stderr, "Insufficient memory\n");
-          return 1;
-      }
-
-      // Populate new node
-      nu->x = number;
-      nu->next = NULL;
-
-      // Adds new node to front of list
-      for (point *ptr = mypoint; ptr != NULL; ptr = ptr->next)
-      {
-          // If the current node has a null pointer, connect new node to it
-          if (ptr->next == NULL)
-          {
-              ptr->next = nu;
-              break;
-          }
-      }
-  }
-
-      // Prints the list
-      for (point *ptr = mypoint; ptr != NULL; ptr = ptr->next)
-          printf("%i ", ptr->x);
-
-      // Free nodes
-      point *track = mypoint;
-      while (track != NULL)
-      {
-          point *ptr = track->next;
-          free(track);
-          track = ptr;
-      }
-
-      return 0;
-}
-
-int get_int(char arr[])
-{
-    printf("%s", arr);
-
-    int i = 0;
-
-    scanf("%i", &i);
-
-    if (feof(stdin))
-        return INT_MAX;
-
-    return i;
+    return 0;
 }
