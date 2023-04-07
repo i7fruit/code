53d60cc74bcd9fed390ad813154142cec2454dc6
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-07 04:18:55 +0000

    Fri, Apr 7, 2023, 12:18 AM -04:00

--- practice/06_pointers/05_dynamic_linked_list.c
+++ practice/06_pointers/05_dynamic_linked_list.c
@@ -1,5 +1,50 @@
 /**
  * 05_dynamic_linked_list.c
  *
- * 
+ * Friday, April 07 2023 00:12hrs
+ *
+ * This program illustrates the dynamic
+ * allocation of memory to build a linked
+ * list.
 */
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
+// Structure definition
+typedef struct entry
+{
+    int value;
+    struct entry *next;
+}
+entry;
+
+int main(void)
+{
+    // Creates a pointer to the start of the list
+    entry *list_start = NULL;
+
+    while (true)
+    {
+        // Prompts user for input
+        int number = get_int("Number: ");
+    }
+
+    return 0;
+}
+
+/******************************************************
+ * get_int()
+*/
+int get_int(char *arr)
+{
+    printf("%s", arr);
+
+    int i = 0;
+
+    scanf("%i", &i);
+
+    // If the user hits ctrl+d, return 2147483547
+    if (feof(stdin))
+        return INT_MAX;
+}
