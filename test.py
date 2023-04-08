b0f55896bf450c79e2d3e10c8d96e4049172ec7c
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-08 02:34:56 +0000

    Fri, Apr 7, 2023, 10:34 PM -04:00

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 #! usr/bin/env python3
-for i in range(10):
-    print(i)
+import sys
+
+values = [12, 98, 0]
+
+while True:
+    number = int(input("number: "))
+    if number == sys.maxsize:
+        break
+
+    values.insert(0, number)
+
+print(values)
