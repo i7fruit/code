1aef68863f6036d0934ecb9e95b5ae9f8c4fe3d3
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-08 02:44:20 +0000

    Fri, Apr 7, 2023, 10:44 PM -04:00

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import sys
 
 def get_int(word):
     i = int(input(word))
-    if i == 2147483647:
-        return i
+    if not i:
+        return sys.maxsize
     return i
 
 values = [12, 98, 0]
 
 while True:
     number = get_int("number: ")
-    if number == 2147483647:
+    if number == sys.maxsize:
         break
 
     values.insert(0, number)
