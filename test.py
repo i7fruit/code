245bfa6682ce640562b5490abb14a44db1162471
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-08 02:41:25 +0000

    Fri, Apr 7, 2023, 10:41 PM -04:00

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 #! usr/bin/env python3
 import sys
 
+def get_int(word):
+    i = int(input(word))
+    if i == 2147483647:
+        return i
+    return i
+
 values = [12, 98, 0]
 
 while True:
-    number = int(input("number: "))
-    if not number:
+    number = get_int("number: ")
+    if number == 2147483647:
         break
 
     values.insert(0, number)
