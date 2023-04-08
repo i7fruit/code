876094753d58eb73f3629cbb643262a75abbc434
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-08 21:26:42 +0000

    Sat, Apr 8, 2023, 5:26 PM -04:00

---
+++ kochan/11_pointers/10_pointer_arithmetic.c
@@ -0,0 +1,15 @@
+/**
+ * 10_pointer_arithmetic.c
+ *
+ * Saturday, April 08 2023 17:24hrs
+ *
+ * This program illustrates the use of
+ * pointer arithmetic to sum up the values
+ * of the elements in an array
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int 
+}
