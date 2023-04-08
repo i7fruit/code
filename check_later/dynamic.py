dc99822667ff0e76570578c254409ee4fd3c85f8
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-08 03:21:48 +0000

    Fri, Apr 7, 2023, 11:21 PM -04:00

---
+++ check_later/dynamic.py
@@ -0,0 +1,27 @@
+#! usr/bin/env python3
+import sys
+
+
+
+def main():
+    values = [12, 98, 0]
+
+    while True:
+        number = get_int("number: ")
+
+        if number == sys.maxsize:
+            break
+
+        values.insert(0, number)
+
+    print(values)
+
+def get_int(word):
+    try:
+        i = int(input(word))
+        return i
+    except:
+        return sys.maxsize
+
+if __name__ == "__main__":
+    main()
