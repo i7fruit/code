5cef4995045781c610a8438f714343c2230fa8f7
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-14 04:20:06 +0000

    Fri, Apr 14, 2023, 12:20 AM -04:00

---
+++ kochan/11_pointers/13_string_length.c
@@ -0,0 +1,19 @@
+/**
+ * 13_string_length.c
+ *
+ * Friday, April 14 2023 00:17hrs
+ *
+ * This program finds the length of a
+ * string by finding the difference between
+ * the the pointer value of the last element
+ * of the string, and the first element of the
+ * string
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    char *word = NULL;
+
+    while (true)
+}
