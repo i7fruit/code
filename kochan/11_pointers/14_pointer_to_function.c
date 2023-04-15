2abc8fe64b577c458bed24b18d445f79fb27a739
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-15 00:33:00 +0000

    Fri, Apr 14, 2023, 8:33 PM -04:00

--- kochan/11_pointers/14_pointer_to_function.c
+++ kochan/11_pointers/14_pointer_to_function.c
@@ -11,5 +11,38 @@
 int main(void)
 {
     int *value = NULL;
-    int *multiplier = 0;
+    int *multiplier = NULL;
+
+    get_values(value, multiplier);
+
+    printf("%i\n%i\n", value, multiplier);
+
+    return 0;
+}
+
+/**********************************************
+ * get_values()
+*/
+void get_values(int *num, int *mult)
+{
+    int i = 0, j = 0;
+
+    num = malloc(sizeof(int));
+    if (!num)
+    {
+        fprintf(stderr, "Insufficient memory\n");
+        return 1;
+    }
+
+    mult = malloc(sizeof(int));
+    if (!mult)
+    {
+        fprintf(stderr, "Insufficient memory\n");
+        return 2;
+    }
+
+    scanf("%i", &i);
+    *num = i;
+    scanf("%i", &j);
+    *mult = j;
 }
