ef0b8d314b791c453ce81deb1ad7db29972a314b
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-15 00:19:19 +0000

    Fri, Apr 14, 2023, 8:19 PM -04:00

---
+++ kochan/11_pointers/14_pointer_to_function.c
@@ -0,0 +1,14 @@
+/**
+ * 14_pointer_to_function.c
+ *
+ * Friday, April 14 2023 20:18hrs
+ *
+ * This program illustrates the use of pointers
+ * to functions.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
