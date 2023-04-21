bcd87aa5f745ce9d5bd2ae2ca254982510f916a1
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-21 03:44:30 +0000

    Thu, Apr 20, 2023, 11:44 PM -04:00

--- kochan/11_pointers/exercise_11/00_insert_node.c
+++ kochan/11_pointers/exercise_11/00_insert_node.c
@@ -24,6 +24,7 @@ entry;
 // Function prototypes
 void get_data(int *id, int *val);
 void print_list(entry *list);
+int build_list(entry *start, int *id, int *value);
 
 int main(void)
 {
@@ -32,7 +33,112 @@ int main(void)
     // Pointer to the start of the linked list
     entry *start = NULL;
 
-    while (true)
+    //while (true)
+    //{
+    //    // Prompts user for data
+    //    printf("Enter an ID and a value: ");
+    //    get_data(&id, &value);
+
+    //    // Stops the loop if the user hits ctrl+d
+    //    if (value == INT_MAX)
+    //    {
+    //        printf("Linked list completed\n");
+    //        break;
+    //    }
+
+    //    // Checks if the ID has previously been used
+    //    bool is_used = false;
+
+    //    for (entry *ptr = start; ptr != NULL; ptr = ptr->next)
+    //    {
+    //        if (ptr->id == id)
+    //        {
+    //            is_used = true;
+    //            break;
+    //        }
+    //    }
+
+    //    // Appends the data to the list if the ID is unique
+    //    if (!is_used)
+    //    {
+    //        // Create new node for data
+    //        entry *nu = malloc(sizeof(entry));
+    //        if (!nu)
+    //        {
+    //            fprintf(stderr, "Insufficient memory\n");
+    //            return 1;
+    //        }
+
+    //        // Initializes the node
+    //        nu->id = id;
+    //        nu->value = value;
+    //        nu->next = NULL;
+
+    //        // Appends node if list isn't null
+    //        if (start)
+    //        {
+    //            nu->next = start;
+    //            start = nu;
+    //        }
+    //        else
+    //            // Appends first node if list is null
+    //            start = nu;
+    //    }
+    //}
+
+    // Prints the list
+    putchar('\n');
+    print_list(start);
+
+    printf("Do you want to insert a new node? (y/n): ");
+
+    // Free nodes
+    entry *ptr = start;
+    while (ptr != NULL)
+    {
+        entry *trav = ptr->next;
+        free(ptr);
+        ptr = trav;
+    }
+
+    return 0;
+}
+
+/*********************************
+ * get_data()
+*/
+void get_data(int *id, int *val)
+{
+    do
+    {
+        scanf("%i %i", id, val);
+
+        // Exits the do-while loop if the user hits ctrl+d
+        if (feof(stdin))
+        {
+
+            *val = INT_MAX;
+            break;
+        }
+    }
+    while (*id < 1);
+}
+
+/***********************************
+ * print_list()
+*/
+void print_list(entry *list)
+{
+    for (entry *trav = list; trav != NULL; trav = trav->next)
+        printf("ID: %3i, Value: %3i\n", trav->id, trav->value);
+}
+
+/***********************************
+ * build_list()
+*/
+int build_list(entry *start, int *id, int *value)
+{
+     while (true)
     {
         // Prompts user for data
         printf("Enter an ID and a value: ");
@@ -85,57 +191,5 @@ int main(void)
         }
     }
 
-    // Prints the list
-    putchar('\n');
-    print_list(start);
-
-    printf("Do you want to insert a new node? (y/n): ");
-
-    // Free nodes
-    entry *ptr = start;
-    while (ptr != NULL)
-    {
-        entry *trav = ptr->next;
-        free(ptr);
-        ptr = trav;
-    }
-
     return 0;
-}
-
-/*********************************
- * get_data()
-*/
-void get_data(int *id, int *val)
-{
-    do
-    {
-        scanf("%i %i", id, val);
-
-        // Exits the do-while loop if the user hits ctrl+d
-        if (feof(stdin))
-        {
-
-            *val = INT_MAX;
-            break;
-        }
-    }
-    while (*id < 1);
-}
-
-/***********************************
- * print_list()
-*/
-void print_list(entry *list)
-{
-    for (entry *trav = list; trav != NULL; trav = trav->next)
-        printf("ID: %3i, Value: %3i\n", trav->id, trav->value);
-}
-
-/***********************************
- * build_list()
-*/
-int build_list(entry *start)
-{
-    
 }
