cf589feddcee2aaa4e88abb27e2cc1aa5da449f4
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-28 20:17:14 +0000

    Fri, Apr 28, 2023, 4:17 PM -04:00

--- test.c
+++ test.c
@@ -11,11 +11,14 @@ node;
 int main(void)
 {
     node one, two, three;
+    node *list = &one;
     one = (node){1, &two};
     two = (node){2, &three};
     three = (node){3, NULL};
 
     printf("%i\n%i\n%i\n", one.val, two.val, three.val);
+
+    insert(list, )
 
     return 0;
 }
