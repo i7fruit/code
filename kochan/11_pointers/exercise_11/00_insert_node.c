3ee39114cd30b4257e47d4740d97701d12056643
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-29 04:00:25 +0000

    Sat, Apr 29, 2023, 12:00 AM -04:00

--- kochan/11_pointers/exercise_11/00_insert_node.c
+++ kochan/11_pointers/exercise_11/00_insert_node.c
@@ -268,7 +268,13 @@ entry *get_position(entry *list)
     {
         static int count = 0;
 
-        if (count + 1 == insertion_point)
+        if (insertion_point == 0)
+            return list;
+
+        else if (insertion_point == node_count)
+            break;
+
+        else if (count == insertion_point)
             return ptr;
 
         count++;
