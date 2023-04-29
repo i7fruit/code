ae1adc071ef81d3c15f430f753bd17902e9f0463
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-04-29 17:51:02 +0000

    Sat, Apr 29, 2023, 1:51 PM -04:00

--- kochan/11_pointers/exercise_11/00_insert_node.c
+++ kochan/11_pointers/exercise_11/00_insert_node.c
@@ -267,15 +267,15 @@ entry *get_position(entry *list)
     insertion_point--;
 
     // Iterates through the list to find a pointer to the node before the insertion point
-    for (entry *ptr = list; ptr != NULL; ptr = ptr->next)
+    for (entry *ptr = list, *prev = list; ptr != NULL; prev = ptr, ptr = ptr->next)
     {
         static int count = 0;
 
         if (insertion_point == 0)
             return list;
 
-        else if ((count + 1 == insertion_point) && (insertion_point != 0))
-            return ptr;
+        else if ((count == insertion_point))
+            return prev;
 
         else if (insertion_point == node_count)
             break;
