c8b2e68acc7bc192dec49e29bcd3ff9887203eb8
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-02 22:14:22 +0000

    Sun, Jul 2, 2023, 10:14 PM +00:00

--- test.c
+++ test.c
@@ -4,8 +4,8 @@ void print(char *arr, int n);
 
 int main(void)
 {
-    char values[] = {"one", "twelve", "three"};
-    char *ptr = values;
+    char values[3][10] = {"one", "twelve", "three"};
+    char **ptr = values;
 
     print(ptr, 3);
 
