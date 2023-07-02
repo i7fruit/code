5cfa9572903f1150f7bbde9d0cd6609209452bcd
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-02 15:14:27 +0000

    Sun, Jul 2, 2023, 3:14 PM +00:00

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void print(int *arr, int n)
+void print(int *arr[], int n)
 {
     for (int i = 0; i < n; i++)
         printf("%i ", *(arr + i));
@@ -9,9 +9,9 @@ void print(int *arr, int n)
 int main(void)
 {
     int values[] = {1, 2, 3};
-    int *ptr = &values;
+    int *ptr = values;
 
-    print(ptr, 3);
+    print(&ptr, 3);
 
     return 0;
 }
