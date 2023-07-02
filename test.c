28511ae6812dadaad244b511037c19ddca180075
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-02 14:02:18 +0000

    Sun, Jul 2, 2023, 2:02 PM +00:00

--- test.c
+++ test.c
@@ -1,51 +1,9 @@
-#include <stdio.h>
-#include <limits.h>
-
-typedef struct node
-{
-    int val;
-    struct node *next;
-}
-node;
-
-node *insert(node *start, node *ptr);
-
 int main(void)
 {
-    node one, two, three;
-    node *list = &one;
-    one = (node){1, &two};
-    two = (node){2, &three};
-    three = (node){3, NULL};
+    char string[] = "ball";
+    char *ptr = string;
 
-    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
-        printf("%i ", ptr->val);
-
-    putchar('\n');
-
-    list = insert(list, list);
-
-    for (node *ptr = list; ptr != NULL; ptr = ptr->next)
-        printf("%i ", ptr->val);
-
-    putchar('\n');
+    printf("%s\n", ptr);
 
     return 0;
-}
-
-node *insert(node *start, node *ptr)
-{
-    node nu = {40, NULL};
-
-    for (node *trav = start; trav != NULL; trav = trav->next)
-    {
-        if (trav == start)
-        {
-            nu.next = start;
-            start = &nu;
-            return start;
-        }
-    }
-
-    return start;
 }
