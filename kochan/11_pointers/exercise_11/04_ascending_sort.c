e7fe894501122bca9404ecdd3db463488f677fab
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-03 04:02:58 +0000

    Mon, Jul 3, 2023, 4:02 AM +00:00

---
+++ kochan/11_pointers/exercise_11/04_ascending_sort.c
@@ -0,0 +1,20 @@
+/**
+ * 04_ascending_sort.c
+ * Sunday, July 02, 23:57hrs
+ *
+ * This program sorts three integers
+ * in ascending order without using
+ * an array.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    short a = 3, b = 1, c = 8;
+
+    if (a > b && a > c)
+        printf("%i ", a);
+    else if (b > a && b > c)
+        printf(")
+}
