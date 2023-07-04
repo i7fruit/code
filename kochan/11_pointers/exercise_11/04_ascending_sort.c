cbf3551cdd19a5f85b3490053a57a30410be72b5
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-04 18:18:39 +0000

    Tue, Jul 4, 2023, 6:18 PM +00:00

---
+++ kochan/11_pointers/exercise_11/04_ascending_sort.c
@@ -0,0 +1,18 @@
+/**
+ * 04_ascending_sort.c
+ *
+ * Tuesday, July 04, 2023 14:16hrs
+ *
+ * This program sorts three numbers
+ * in ascending order without the use
+ * of arrays
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    // Variables to store three integers
+    int a = 1, b = 2, c = 0;
+
+    // First finds
+}
