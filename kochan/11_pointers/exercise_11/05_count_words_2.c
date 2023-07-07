7d084c4017fb8993488210c4d384dbe9ce97a7de
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-07 03:35:58 +0000

    Fri, Jul 7, 2023, 3:35 AM +00:00

--- kochan/11_pointers/exercise_11/05_count_words_2.c
+++ kochan/11_pointers/exercise_11/05_count_words_2.c
@@ -9,49 +9,72 @@
  * It also identifies integers separated by
  * commas
  */
+/**
+ * 07_null_string.c
+ *
+ * Wednesday, August 17 2022 22:15hrs
+ *
+ * This program counts the number of words in a string
+ * of text that spans several lines. The user taps the
+ * Return/Enter key twice to indicate to the program that
+ * data entry has ended.
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
 
-// Function prototypes
-void readline(char *arr, int limit);
+void readline(char arr[], int limit);
 int count_words(char array[]);
 
 int main(void)
 {
     const short size = 64;
-    char string[size];
-    char *ptr = string;
+    int count = 0;
 
-    printf("Enter a string of text\n");
-    readline(ptr, size - 1);
+    bool is_reading = true;
 
-    int word_count = count_words(string);
+    // Array to store string of text entered by the user
+    char text[size];
 
-    printf("The string contains %i words.\n", word_count);
+    printf("Enter text\n\n");
+
+    while (is_reading)
+    {
+        readline(text, size - 1);
+
+        // Checks for the null string
+        if (text[0] == '\0')
+            is_reading = false;
+
+        else
+            // Counts words in each line
+            count += count_words(text);
+    }
+
+    printf("Count = %i\n", count);
 
     return 0;
 }
 
 /****************************************************
- * The readline() function takes a pointer to a
- * character array to store the string captured from standard input,
+ * The readline() function takes a character array
+ * to store the string captured from standard input,
  * and a second argument of an integer value representing
  * the number of characters the user is limited to storing
  * in the array. This way the user does not exceed the buffer.
  */
-void readline(char *arr, int limit)
+void readline(char arr[], int limit)
 {
     short i = -1;
 
     do
     {
-        *(arr + (++i)) = getchar();
+        arr[++i] = getchar();
     }
-    while (*(arr + i) != '\n' && i != limit);
+    while (arr[i] != '\n' && i != limit);
 
     // Overwrites '\n' with '\0'
-    *(arr + i) = '\0';
+    arr[i] = '\0';
 }
 
 /*******************************************************
@@ -69,13 +92,13 @@ int count_words(char array[])
 
     for (short i = 0; array[i] != '\0'; i++)
     {
-        if ((isalpha(array[i]) || isdigit(array[i])) && !is_counting)
+        if (isalpha(array[i]) && !is_counting)
         {
             is_counting = true;
             count++;
         }
 
-        else if (!isalpha(array[i]) && !isdigit(array[i]) && array[i] != '\'' && array[i] != ',')
+        else if (!isalpha(array[i]))
             is_counting = false;
     }
 
