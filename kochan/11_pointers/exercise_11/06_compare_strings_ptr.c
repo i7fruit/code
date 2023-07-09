d1199da9c7361e7ca3cdefe002f2df8c00a696df
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-09 01:16:57 +0000

    Sun, Jul 9, 2023, 1:16 AM +00:00

---
+++ kochan/11_pointers/exercise_11/06_compare_strings_ptr.c
@@ -0,0 +1,17 @@
+/**
+ * 06_compare_strings_ptr.c
+ *
+ * Saturday, July 08 2023 21:18hrs
+ *
+ * This program compares two strings
+ * to determine if they're equal, or
+ * if one comes before the other alphabetically
+ *
+ * This function uses pointers.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
