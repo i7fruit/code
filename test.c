6f1ab85b6ae9578f75e22110013186b593d8b925
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-10 00:44:36 +0000

    Mon, Jul 10, 2023, 12:44 AM +00:00

--- test.c
+++ test.c
@@ -3,12 +3,12 @@
 int main(void)
 {
     const int size = 64;
-    char string0[size] = "Hi";
-    char string1[size] = "Hi";
+    char string0[size] = "ai";
+    char string1[size] = "aj";
 
     int i = 0;
 
-    while (string0[i] == string1[i]) 
+    while ((string0[i] == string1[i]) && string0[i] != '\0')
         i++;
 
     if (string0[i] == string1[i])
