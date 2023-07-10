72eca325aa72fccdbaf5f416843c506af6c0debb
Nzefili Macaulay <i7fruit@users.noreply.github.com>
2023-07-10 00:34:13 +0000

    Mon, Jul 10, 2023, 12:34 AM +00:00

--- test.c
+++ test.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 
-void print(char *arr[], int n);
-
 int main(void)
 {
-    char values[][10] = {"one", "two", "three"};
-    char *ptr = values;
+    char *string = NULL;
+    int capacity = 0;
 
-    print(ptr, 3);
-
-    return 0;
-}
-
-void print(char *arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-       for(int j = 0; *(arr + (i*j)) != '\0'; j++)
-            printf("%c ", *(arr + (i*j));
-
-        putchar('\0');
+    while (true)
+    {
+        
+    }
 }
